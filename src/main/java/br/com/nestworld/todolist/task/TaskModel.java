9db1cdb1537496aa0b076be51f75529d92217c9b
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2023-12-14 21:58:59 -0300

    fim do projeto

--- src/main/java/br/com/nestworld/todolist/task/TaskModel.java
+++ src/main/java/br/com/nestworld/todolist/task/TaskModel.java
@@ -31,5 +31,12 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createAt;
 
+    public void setTitle(String title) throws Exception{
+        if(title.length()>50){
+            throw new Exception("O campo title deve conter no maximo 50 caracteres");
+        }
+        this.title = title;
+    }
+
     
 }
